3fa11833d3f7a88d3c29aa0011c70f5b67a0b576
Raul Valls <valls.raul@gmail.com>
2016-02-26 13:42:33 +0100

    v1.3 dropshadow contact menu with links

---
+++ js/code.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Raul on 26/02/2016.
+ */
+
+function changePhone(){
+    document.getElementById("aPhone").innerHTML = "606602238";
+}
+
+function changeTelegram(){
+    document.getElementById("aTele").innerHTML = "@rul3s";
+}
